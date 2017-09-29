f697f01877c3f6dd3b5ce606227bd875a7aa06a7
Paul ReFalo <paul.refalo@gmail.com>
2017-09-29 16:56:55 -0700

    docs:  Finished while.py
    Finished while.py using while loop for Fizz Buzz assignment

---
+++ for.py
@@ -0,0 +1,23 @@
+# CS 519 assignment 2 - Fizz Buzz with for loop by Paul ReFalo
+
+number = int(input("Enter a number: ")) # get user input
+
+i = 1                                   # seed increment variable 'i' with value of 1
+
+while i <= number:
+    if (i % 3) and (i % 5):             # has remainder for div by 3 and div by 5
+        print(i)
+    elif (i % 3) or (i % 5):            # evenly divisible by 3 or 5
+        if (i % 3):                     # evenly divisible by 5
+            print(str(i) + " Buzz")
+        elif (i % 5):                   # evenly divisible by 3
+            print(str(i) + " Fizz")
+        else:
+            print("This shouldn't happen")
+    else:                               # must be evenly divisible by both 3 and 5
+        print(str(i) + " Fizz Buzz")
+    i += 1
+
+print("Done!")
+
+
