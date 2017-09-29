772a988f30d44231e2e0ccf5b8fa64d69dd3ca78
Paul ReFalo <paul.refalo@gmail.com>
2017-09-29 08:49:12 -0700

    docs:  Add repo for CS519
    CS519 - Programming concepts for non-majors

---
+++ while.py
@@ -0,0 +1,30 @@
+# CS 519 assignment 2 - Fizz Buzz by Paul ReFalo
+
+number = int(input("Enter a number: "))
+print(type(number))
+
+
+
+'''
+import platform
+
+print("You are running Python version %s." % platform.python_version())
+user = input("What is your name? ")
+currentAge = input("How old are you now? ")
+retirementAge = input("How old will you be when you retire? ")
+currentlySaved = input("How much have you saved for retirement? ")
+annualSavings = input("How much are you saving per year? ")
+savings = int(currentlySaved) + (int(retirementAge) - int(currentAge)) * int(annualSavings)
+print(user + ", you will have $" + str(savings) + " if you keep saving at the current rate.")
+'''
+
+# Example output
+'''
+You are running Python version 3.5.1.
+What is your name? Paul ReFalo
+How old are you now? 48
+How old will you be when you retire? 70
+How much have you saved for retirement? 42000
+How much are you saving per year? 500
+Paul ReFalo, you will have $53000 if you keep saving at the current rate.
+'''
