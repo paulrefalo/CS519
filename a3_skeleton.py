5f509f3e6a9e72bf518d2c0c33001c5c1981d4e2
Paul ReFalo <paul.refalo@gmail.com>
2017-10-12 23:22:45 -0700

    docs:  Finished module 3
    Also added a tutorial on mapping to each element

--- a3_skeleton.py
+++ a3_skeleton.py
@@ -15,19 +15,18 @@ import operator
 
 students = []   # declare empty students list to contain student dictionaries
 
-student1 = {'id':12345, 'first_name':'John', 'last_name':'Lennon', 'assignments':[('assignment_1',0),('assignment_2',2),('assignment_3',4)]}
+student1 = {'id':12345, 'first_name':'John', 'last_name':'Lennon', 'assignments':[('assignment_1',0),('assignment_2',2),('assignment_3',3)]}
 student2 = {'id':12346, 'first_name':'Paul', 'last_name':'McCartney', 'assignments':[('assignment_1',2),('assignment_2',3),('assignment_3',2)]}
-student3 = {'id':12347, 'first_name':'Ringo', 'last_name':'Starr', 'assignments':[('assignment_1',3),('assignment_2',1),('assignment_3',1)]}
+student3 = {'id':12347, 'first_name':'Ringo', 'last_name':'Starr', 'assignments':[('assignment_1',2),('assignment_2',1),('assignment_3',1)]}
 student4 = {'id':12348, 'first_name':'George', 'last_name':'Harrison', 'assignments':[('assignment_1',3),('assignment_2',4),('assignment_3',3)]}
-student5 = {'id':12349, 'first_name':'Pete', 'last_name':'Best', 'assignments':[('assignment_1',4),('assignment_2',2),('assignment_3',4)]}
+student5 = {'id':12349, 'first_name':'Pete', 'last_name':'Best', 'assignments':[('assignment_1',4),('assignment_2',2),('assignment_3',3)]}
+# student 6 added for testing but not added to list because Tom Petty is not one of the Beatles
+student6 = {'id':12350, 'first_name':'Tom', 'last_name':'Petty', 'assignments':[('assignment_1',4),('assignment_2',2),('assignment_3',4)]}
 
+
+# add multiple elements to list with extend
 students.extend((student5, student2, student3, student4, student1))     # students added out of order then sorted for testing
 students.sort(key=itemgetter("id"))
-
-# pprint(students)
-# print("Number of assignments: " + str(numOfAssignments))
-# print(s["assignments"][1][1])
-# print(s["assignments"][num][1])
 
 
 #This should return the average grade of all assignments from all students
@@ -45,7 +44,10 @@ def average_grade(students):
     return gradesAverage
 
 averageOfAllGrades = average_grade(students)        # call function and report the result
+print("Called function averageOfAllGrades *******************************************")
 print(str("The average grade of all assignments of all the students is: " + str(averageOfAllGrades)))
+print("\n")
+
 
 #This function should return a list of the n student dictionaries with the
 #highest grades on the assignment passed in as assignment name
@@ -58,10 +60,8 @@ def highest_n_grades(students, assignment_name, n):
     assignmentIndex = -1
 
     for num in range(0, numOfAssignments):
-        print(students[0]["assignments"][num][0])
         if students[0]["assignments"][num][0] == assignment_name:
             assignmentIndex = num
-            print("Yes, found index:  " + str(assignmentIndex))
             break
 
     if assignmentIndex < 0:                                 # check to be sure we found the assignment
@@ -75,9 +75,8 @@ def highest_n_grades(students, assignment_name, n):
         score = s["assignments"][assignmentIndex][1]
         assignmentScores.append([id, score])
 
+    # students list previously sorted by Id in ascending order
     sortList = sorted(assignmentScores, key=operator.itemgetter(1), reverse=True)
-
-    pprint(sortList)
 
     listOfHighest = []
     for e in sortList:
@@ -88,8 +87,11 @@ def highest_n_grades(students, assignment_name, n):
 
     return listOfHighest[:n]                                # return appropriate slice of list
 
-returnedList = highest_n_grades(students, "assignment_2", 3)
+returnedList = highest_n_grades(students, "assignment_2", 2)
+print("Called function returnedList *************************************************")
+print("The returned List of Highest Grades for this assigment is: ")
 pprint(returnedList)
+print("\n")
 
 
 #This function should accept a student dictionary, a string representing
@@ -97,7 +99,30 @@ pprint(returnedList)
 #the assignment and grade should be added to the end of the list of assignments. If
 #this was successful it should return true, otherwise it should return false.
 
-#def add_grade(student, assignment_name, grade):
+def add_grade(student, assignment_name, grade):
+    studentID = student["id"]
+    assignmentsTuples = student["assignments"]
+    assignmentsList = []
+
+    for tup in assignmentsTuples:
+        assignmentsList.append(tup[0])
+
+    if assignment_name not in assignmentsList:
+        for s in students:                          # find student by ID and append assign list for that student
+            if s["id"] == studentID:
+                s["assignments"].append((assignment_name, grade))
+                return True
+        # Shouldn't happen unless you passed in a student not in our list
+        print("Student not found in master students list")
+
+    return False
+
+print("Calling function add_grade ****************************************************")
+addGradeResult = add_grade(student3, "assignment_4", 3)
+if addGradeResult:
+    print("The result of add_grade is True\n")
+else:
+    print("The result of add_grade is False\n")
 
 
 #This function should accept a student dictionary, a string
@@ -106,12 +131,60 @@ pprint(returnedList)
 #and updated the function should return true, otherwise it should return false.
 #The order of assignments should be preserved.
 
-#def update_grade(student, assignment_name, grade):
+def update_grade(student, assignment_name, grade):
+    studentID = student["id"]
+    assignmentsTuples = student["assignments"]
+    assignmentsList = []
+
+    for tup in assignmentsTuples:
+        assignmentsList.append(tup[0])
+
+    if assignment_name in assignmentsList:
+        for s in students:  # find student by ID and append assign list for that student
+            if s["id"] == studentID:
+                studentAssignments = s["assignments"]
+                for idx, a in enumerate(studentAssignments):
+                    if a[0] == assignment_name:
+                        s["assignments"][idx] = (assignment_name, grade)
+                        return True
+
+        # Shouldn't happen unless you passed in a student not in our list
+        print("Student not found in master students list")
+
+    return False
+
+print("Calling function update_grade *************************************************")
+updateGradeResult = update_grade(student5, 'assignment_2', 4)
+if updateGradeResult:
+    print("The result of update_grade is True\n")
+else:
+    print("The result of update_grade is False\n")
 
 
 #Write a function called passing_student_ids which accepts as an argument a list
 #of students. It should return a list of student ids which represent students
 #having an average grade on all of their assignments which is >= 2.0
 
+def passing_student_ids(students):
+    passingStudents = []
 
-'''John Lennon, Paul McCartney, Ringo Starr, George Harrison, Pete Best, Stuart Sutcliffe'''
+    for s in students:                              # loop over all students and append their grades to grades list
+        grades = []
+        numOfAssignments = len(s["assignments"])    # get length of assignments list of tuples to set range
+        for num in range(0, numOfAssignments):
+            grades.append(s["assignments"][num][1]) # get the second element of the tuple and append grades list
+
+        print(s["first_name"])
+        print(sum(grades))
+        print(len(grades))
+        print(sum(grades) / len(grades))
+        if (sum(grades) / len(grades) >= 2.0):
+            passingStudents.append(s["id"])
+
+
+    return passingStudents       # list of IDs of students with grade avg >= 2.0
+
+print("Calling function passing_student_ids *******************************************")
+passingStudentsIDs = passing_student_ids(students)
+print("The list of students passing have IDs: ")
+pprint(passingStudentsIDs)
