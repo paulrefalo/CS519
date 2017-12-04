de84ad27236238e1ff10d83faa1a304826923a1b
Paul ReFalo <paul.refalo@gmail.com>
2017-12-04 14:22:07 -0800

    docs:  Done with final exam
    Exam and quarter complete.

---
+++ ExamTesting.py
@@ -0,0 +1,23 @@
+
+a = 1
+b = 'rabbit'
+c = 3
+
+mutableList = [a, b, c]
+print('mutableList is: ', mutableList)
+
+immutableTuple = (a, b, c)
+print('immutableTuple is: ', immutableTuple)
+
+
+mutableList[1] = 9
+print(mutableList)
+
+#immutableTuple[1] = 9
+print(immutableTuple[1])
+
+st = "Sesame Street"
+
+
+x = '''Marcus Aurelis said "A man's worth is no greater than his ambitions."'''
+print(x)
